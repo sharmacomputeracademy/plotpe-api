f3c425862958af48387f249b3cea9b93ea3905a9
SCALive <sharma.computer.academy@gmail.com>
2026-04-16 19:00:15 +0530

    set cors;

--- api/index.js
+++ api/index.js
@@ -13,10 +13,12 @@ connectDB();
 
 const app = express();
 
-app.use(cors({
-  origin: "https://plotpe.vercel.app",
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: "https://plotpe-two.vercel.app/",
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 app.use(cookieParser());
