cf44d3a1f171aa4cc0721e606393d4dfb9391c26
sharmacomputeracademy <sharma.computer.academy@gmail.com>
2026-04-27 11:44:15 +0000

    Update listingRoute.js

--- api/routes/listingRoute.js
+++ api/routes/listingRoute.js
@@ -13,7 +13,7 @@ const listingRouter = express.Router();
 listingRouter.post("/create", verifyToken, createListing);
 listingRouter.delete("/delete/:id", verifyToken, deleteListing);
 listingRouter.put("/update/:id", verifyToken, updateListing);
-listingRouter.get("/get/:id", getListing);
 listingRouter.get("/get", getListings);
+listingRouter.get("/get/:id", getListing);
 
 module.exports = listingRouter;
